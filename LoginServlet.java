a3e6e3d0fe61cd181f3532566899f738208f7866
akashhulbutti-Thinkitive <77093348+akashhulbutti-Thinkitive@users.noreply.github.com>
2021-01-27 00:19:45 +0530

    Login Veriication Assignment
    Simple User Login Verification using JDBC mysql in JSP Servlet

---
+++ LoginServlet.java
@@ -0,0 +1,45 @@
+package com.akash;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public LoginServlet() {
+		super();
+
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		RequestDispatcher dispatcher = null;
+
+		if (new VerifyUser().checkCredentials(username, password)) {
+			dispatcher = getServletContext().getRequestDispatcher("/success.html");
+
+		} else {
+			dispatcher = getServletContext().getRequestDispatcher("/error.html");
+
+		}
+
+		dispatcher.forward(request, response);
+
+	}
+
+}
